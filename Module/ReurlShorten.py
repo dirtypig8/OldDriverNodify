6936ac5ed382b3cb6959a0e9e26fde63b93138d7
dirtypig8 <dirtypig8@gmail.com>
2019-12-13 01:30:40 +0800

    -修正 短網址URL有時會500，導致程式異常

--- Module/ReurlShorten.py
+++ Module/ReurlShorten.py
@@ -5,8 +5,7 @@ from CoreConfig.ConfigDictionary import ConfigDictionary
 
 class ReurlShorten:
     def __init__(self):
-        # self.key = ConfigDictionary.config_dict['reurl_key']
-        self.key = '4070df69d794ea3c104b353100ba214de0d7be378d894494ab38acc62b055f6689'
+        self.key = ConfigDictionary.config_dict['reurl_key']
         self.api_url = 'https://api.reurl.cc/shorten'
         self.Net = Net()
 
@@ -22,16 +21,16 @@ class ReurlShorten:
         try:
             response = self.Net.Post(url=self.api_url, header=header, data=json.dumps(data))
             response = json.loads(response)
+            if response:
+                if response["res"] == 'success':
+                    return response['short_url']
+            return url
 
         except Exception as e:
             print("ReurlShorten fail '{}'".format(e))
             return url
 
-        else:
-            if response:
-                if response["res"] == 'success':
-                    return response['short_url']
-            return url
+
 
 
 
